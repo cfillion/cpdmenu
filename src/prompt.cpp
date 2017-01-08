bee945313fcd42064dd0ff7237522466955e4098
cfillion <cfillion@users.noreply.github.com>
2017-01-08 09:19:54 -0500

    fix tab and shift+tab handling

--- src/prompt.cpp
+++ src/prompt.cpp
@@ -87,11 +87,11 @@ void Prompt::keyPressEvent(QKeyEvent *event)
     Q_EMIT editingFinished({});
     break;
   case Qt::Key_Tab:
-    break;
   case Qt::Key_Right:
   case Qt::Key_Down:
     m_completer->move(1);
     break;
+  case Qt::Key_Backtab:
   case Qt::Key_Left:
   case Qt::Key_Up:
     m_completer->move(-1);
